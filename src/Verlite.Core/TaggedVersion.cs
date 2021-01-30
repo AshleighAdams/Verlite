13b91f329e3b587e4db766f08ab26c363ec7852f
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2021-01-30 23:49:01 +0000

    Added equality checking to TaggedVersion.

--- src/Verlite.Core/TaggedVersion.cs
+++ src/Verlite.Core/TaggedVersion.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
 namespace Verlite
 {
-	public class TaggedVersion
+	public class TaggedVersion : IEquatable<TaggedVersion?>
 	{
 		public SemVer Version { get; }
 		public Tag Tag { get; }
@@ -9,5 +13,17 @@ namespace Verlite
 			Version = version;
 			Tag = tag;
 		}
+
+		public override bool Equals(object? obj) => Equals(obj as TaggedVersion);
+
+		public bool Equals(TaggedVersion? other) =>
+			other is not null &&
+			Version.Equals(other.Version) &&
+			Tag.Equals(other.Tag);
+
+		[ExcludeFromCodeCoverage]
+		public override int GetHashCode() => Version.GetHashCode() ^ Tag.GetHashCode();
+		public static bool operator ==(TaggedVersion left, TaggedVersion right) => EqualityComparer<TaggedVersion>.Default.Equals(left, right);
+		public static bool operator !=(TaggedVersion left, TaggedVersion right) => !(left == right);
 	}
 }
