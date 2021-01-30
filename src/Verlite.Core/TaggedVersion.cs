15baa50c745c7fa3ab5cb5122d28ab554f2645c6
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2021-01-30 19:13:29 +0000

    Updated unshipped API + moved nested class to root namespace.

--- src/Verlite.Core/TaggedVersion.cs
+++ src/Verlite.Core/TaggedVersion.cs
@@ -1,16 +1,13 @@
-﻿namespace Verlite
+namespace Verlite
 {
-	public static partial class HeightCalculator
+	public class TaggedVersion
 	{
-		public class TaggedVersion
+		public SemVer Version { get; }
+		public Tag Tag { get; }
+		public TaggedVersion(SemVer version, Tag tag)
 		{
-			public SemVer Version { get; }
-			public Tag Tag { get; }
-			public TaggedVersion(SemVer version, Tag tag)
-			{
-				Version = version;
-				Tag = tag;
-			}
+			Version = version;
+			Tag = tag;
 		}
 	}
 }
