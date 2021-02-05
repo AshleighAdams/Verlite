377f1865079ab81d6c5dcde4f66acd69925ff006
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2021-02-05 01:58:53 +0000

    Fixed integration tests.

--- tests/IntegrationTests/tests/no-commits.sh
+++ tests/IntegrationTests/tests/no-commits.sh
@@ -6,4 +6,6 @@ dotnet tool install Verlite.CLI --version 0.0.0 > /dev/null
 
 git init > /dev/null
 
-[[ $(dotnet verlite $VERBOSE) == "v0.1.0-alpha.1" ]]
+[[ $(dotnet verlite $VERBOSE) == "0.1.0-alpha.1" ]]
+[[ $(dotnet verlite --min-version 1.2.3 $VERBOSE) == "1.2.3-alpha.1" ]]
+[[ $(dotnet verlite --min-version 1.2.3-beta.4 $VERBOSE) == "1.2.3-beta.4.1" ]]
